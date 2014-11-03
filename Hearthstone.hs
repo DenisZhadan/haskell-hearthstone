0e712f69fc8cb5e2611fe134f72918062656c598
DenisZhadan <denis@ut.ee>
2014-11-03 23:19:14 +0200

    constructors

--- Hearthstone.hs
+++ Hearthstone.hs
@@ -7,6 +7,7 @@ import Data.Array
 Denis Zhadan
 2014-11-03
 -}
+
 -- structure of file 
 type File = [Card] 
 type Card = (Name, Cost, CardType)
@@ -17,10 +18,7 @@ type Cost = Int
 type Deck = [CardType]
 data CardType = MinionCard [Effect] HealthPoint AttackPoint IsTaunt (Maybe MinionType)
               | SpellCard [Effect]
-
---instance Show CardType where
---  show MinionCard e h a t m = "MinionCard" ++ show h
---  show (SpellCard Effect) = "SpellCard" ++ show 5 
+              deriving (Show, Eq, Ord, Read)
 
 type HealthPoint = Int -- number of health points
 type AttackPoint = Int -- number of attack points
@@ -28,52 +26,32 @@ type IsTaunt = Bool -- taunting the creature
 
 -- olenditel võivad olla sellised tüübid
 data MinionType = Beast | Murloc 
-                deriving (Show, Eq, Ord, Enum)
+                deriving (Show, Eq, Ord, Enum, Read)
 
 data Effect = OnPlay     [EventEffect]  -- effekt kaardi käimisel
             | UntilDeath [EventEffect]  -- effekt mis kestab välja käimisest kuni olendi surmani
             | OnDamage   [EventEffect]  -- effekt mis tehakse olendi vigastamisel
             | OnDeath    [EventEffect]  -- effekt mis tehakse olendi tapmisel (elupunktid <= 0)
-
+            deriving (Show, Eq, Ord, Read)
 
 -- toime on kaardi võtmine või olendite mõjutamine --- vastavalt filtrile
 data EventEffect = All [Filter] [CreatureEffect]   -- mõjutatake kõiki filtrile vastavaid olendeid
                  | Choose [Filter] [CreatureEffect]  -- mõjutatake üht kasutaja valitud filtrile vastavat olendeit
                  | Random [Filter] [CreatureEffect]  -- mõjutatake üht juhuslikku filtrile vastavat olendeit
                  | DrawCard  -- toime olendi omanik võtab kaardi
+                 deriving (Show, Eq, Ord, Read)
 
 data CreatureEffect = Health Type HealthPoint  -- elupunktide muutmine
                     | Attack Type AttackPoint  -- ründepunktide muutmine
-                    | Taunt IsTaunt         -- mõnituse muutmine
-                    deriving (Read)
+                    | Taunt  IsTaunt           -- mõnituse muutmine
+                    deriving (Show, Eq, Ord, Read)
+{-
 instance Show CreatureEffect where
   show (Health t p) = "Health: " ++ show t ++ " " ++ show p
   show (Attack t p) = "Attack: " ++ show t ++ " " ++ show p 
   show (Taunt t) = "Taunt: " ++ show t 
-
-{-
-instance Read CreatureEffect where
-  readsPrec _ v = readHealth v ++ readAttack v ++ readTaunt v
-    where 
-      tryParse [] = []
-      tryParse ((attempt, result):xs) =
-            if (take (length attempt) value) == attempt
-            then [(result, drop (length attempt) value)]
-            else tryParse xs
 -}
-{-
-instance (Read a) => Read (SomeType a) where
-    readsPrec d r = readMix r ++ readType r
-      where
-        readMix = readParen True $ \r -> do
-            (v1, r'') <- readsPrec d r
-            (v2, r')  <- readsPrec d r''
-            return (Mix v1 v2, r')
 
-        readType r = do
-            (v, r') <- readsPrec d r
-            return (Type v, r')
--}
 -- muutuse tüüp
 data Type = Relative -- negatiivne relatiivne muutmine on vigastamine 
           | Absolute -- absoluutne negatiivne muutmine ei ole vigastamine
@@ -87,19 +65,39 @@ data Filter = AnyCreature     -- olendid
             | Self            -- mõjutav olend ise
             | Not [Filter]    -- eitus
             | Any [Filter]    -- disjunktsioon: kui üks tingimus on taidetud
+            deriving (Show, Eq, Ord, Read)
+
 readDeck name 
   = readFile name 
 
-readDeck2 name 
+
+readAndIndex fileName 
   = do
-    content <- readFile name
-    putStr content
+    text <- readFile fileName
+    return text
+
+toDeck a
+  = read a :: [CardType]
+
+  
+readFromFile
+  = do
+    text <- readFile "deck0.txt"
+    let content = text
+    --putStr text
     return content
+    --y <- (read (content :: Char) :: [CardType])
+    --a <- (readFile "deck0.txt") :: IO [Char]
+    --let a = "[MinionCard [] 6 7 False Nothing,MinionCard [OnPlay [Choose [] [Health Relative (-1)]]] 4 5 False Nothing,MinionCard [OnPlay [DrawCard]] 2 4 False Nothing]" 
+    --b <- read "[MinionCard [] 6 7 False Nothing,MinionCard [OnPlay [Choose [] [Health Relative (-1)]]] 4 5 False Nothing,MinionCard [OnPlay [DrawCard]] 2 4 False Nothing]" :: [CardType]
+    --a <- readFile "deck0.txt" 
+    --b <- read c :: [CardType]
+    --return b
 
 readDeck3 name 
   = do
     content <- readFile name
-    return content --(read content :: CardType) 
+    return (read content :: [CardType]) 
 
 --main = do
 --    content <- readFile "deck1.txt"
